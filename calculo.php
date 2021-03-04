41deda855c77884ac1c71d7b10fc4b1a02cf8eae
Ethan <etanr25@gmail.com>
2021-03-03 20:51:25 -0600

    Static function

--- calculo.php
+++ calculo.php
@@ -22,22 +22,22 @@
     }
 
     class operaciones{
-        public function sumar($num1,$num2){
+        public static function sumar($num1,$num2){
         $suma= $num1 + $num2;
         return $suma;
         }
 
-        public function restar($num1,$num2){
+        public static function restar($num1,$num2){
         $resta= $num1 - $num2;
         return $resta;
         }
 
-        public function multiplicar($num1,$num2){
+        public static function multiplicar($num1,$num2){
         $multi= $num1 * $num2;
         return $multi;
         }
 
-        public function divicion($num1,$num2){
+        public static function divicion($num1,$num2){
         $dividir= $num1 / $num2;
         return $dividir;
         }
